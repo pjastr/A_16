3763742e6a8b46b57aab1a4ba0d4a818aa84f18d
Piotrek <@>
2017-10-26 12:35:23 +0200

    przesłanie plików

---
+++ A_16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź zdanie i naciśnij Enter.");
+            string zdanie = Console.ReadLine();
+            string wynik = "";
+            string[] slowa = zdanie.Split(new[] { " " }, StringSplitOptions.None);
+            for (int i = slowa.Length - 1; i >= 0; i--)
+            {
+                wynik += slowa[i] + " ";
+            }
+            Console.WriteLine("Odwrócna kolejność:");
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Napisz program, który odwraca kolejność wyrazów w zdaniu. Zdanie ma być wprowadzone z klawiatury, wynik ma być wyświetlony na konsoli.
+
+Przykładowe wejście:
+
+Życie jest jak pudełko czekoladek, nigdy nie wiesz, co ci się trafi.
+
+Przykładowe wyjście:
+
+trafi. się ci co wiesz, nie nigdy czekoladek, pudełko jak jest Życie
